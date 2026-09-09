7b30baf65d6ca4419d9db13e7daccaeaaa77ce9b
kyru <alvintheman703@gmail.com>
2026-09-09 19:10:56 +0300

    Create dedicated clock display page

---
+++ pages/clock.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import DigitalClock from '../components/DigitalClock';
+
+export default function ClockPage() {
+  return (
+    <div className="w-full">
+      <DigitalClock />
+    </div>
+  );
+}
